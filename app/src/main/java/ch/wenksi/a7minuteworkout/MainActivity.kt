12fa51fe71cc75100af578cb12b87e440f5dc792
Silvan Wenk <silvan.wenk@stud.hslu.ch>
2021-08-12 17:12:00 +0200

    Import images and add button and logo to start screen

--- app/src/main/java/ch/wenksi/a7minuteworkout/MainActivity.kt
+++ app/src/main/java/ch/wenksi/a7minuteworkout/MainActivity.kt
@@ -2,11 +2,17 @@ package ch.wenksi.a7minuteworkout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        ll_start.setOnClickListener {
+            Toast.makeText(this, "Here we will start the exercise", Toast.LENGTH_SHORT).show()
+        }
     }
 }
