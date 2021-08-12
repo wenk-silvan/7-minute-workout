012b6452db2e57eb6ea847ac353df0893a9a04fc
Silvan Wenk <silvan.wenk@stud.hslu.ch>
2021-08-12 16:50:58 +0200

    Initial commit - empty activity

---
+++ app/src/main/java/ch/wenksi/a7minuteworkout/MainActivity.kt
@@ -0,0 +1,12 @@
+package ch.wenksi.a7minuteworkout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
