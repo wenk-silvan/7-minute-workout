8681f5f47a67d3fd5323489042b155f8c454a7fa
Silvan Wenk <silvan.wenk@stud.hslu.ch>
2021-08-13 11:08:56 +0200

    Alternating exercise and rest timer

--- app/src/main/java/ch/wenksi/a7minuteworkout/ExerciseActivity.kt
+++ app/src/main/java/ch/wenksi/a7minuteworkout/ExerciseActivity.kt
@@ -3,12 +3,14 @@ package ch.wenksi.a7minuteworkout
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
-import android.widget.Toast
+import android.view.View
 import kotlinx.android.synthetic.main.activity_exercise.*
 
 class ExerciseActivity : AppCompatActivity() {
     private var restTimer: CountDownTimer? = null
     private var restProgress = 0
+    private var exerciseTimer: CountDownTimer? = null
+    private var exerciseProgress = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,25 +33,50 @@ class ExerciseActivity : AppCompatActivity() {
     }
 
     private fun setRestProgressBar() {
-        progressBar.progress = this.restProgress
+        pb_rest.progress = this.restProgress
         this.restTimer = object: CountDownTimer(10000, 1000) {
             override fun onTick(millisUntilFinished: Long) {
                 restProgress++
-                progressBar.progress = 10 - restProgress
-                tv_timer.text = (10 - restProgress).toString()
+                pb_rest.progress = 10 - restProgress
+                tv_timer_rest.text = (10 - restProgress).toString()
             }
 
-            override fun onFinish() {
-                Toast.makeText(this@ExerciseActivity, "Here now we start the exercise", Toast.LENGTH_SHORT).show()
+            override fun onFinish() = setupExerciseView()
+        }.start()
+    }
+
+    private fun setExerciseProgressBar() {
+        pb_exercise.progress = this.exerciseProgress
+        this.exerciseTimer = object: CountDownTimer(30000, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                exerciseProgress++
+                pb_exercise.progress = 30 - exerciseProgress
+                tv_timer_exercise.text = (30 - exerciseProgress).toString()
             }
+
+            override fun onFinish() = setupRestView()
         }.start()
     }
 
     private fun setupRestView() {
+        ll_exercise_view.visibility = View.INVISIBLE
+        ll_rest_view.visibility = View.VISIBLE
+
         if (this.restTimer != null) {
             this.restTimer!!.cancel()
             this.restProgress = 0
         }
         this.setRestProgressBar()
+    }
+
+    private fun setupExerciseView() {
+        ll_rest_view.visibility = View.INVISIBLE
+        ll_exercise_view.visibility = View.VISIBLE
+
+        if (this.exerciseTimer != null) {
+            this.exerciseTimer!!.cancel()
+            this.exerciseProgress = 0
+        }
+        this.setExerciseProgressBar()
     }
 }
