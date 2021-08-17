be9fdd98531d6966333dcc0316d35bde9ab86ad8
Silvan Wenk <silvan.wenk@stud.hslu.ch>
2021-08-17 11:27:42 +0200

    Change exercise status bubbles background

--- app/src/main/java/ch/wenksi/a7minuteworkout/ExerciseActivity.kt
+++ app/src/main/java/ch/wenksi/a7minuteworkout/ExerciseActivity.kt
@@ -75,6 +75,8 @@ class ExerciseActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
 
             override fun onFinish() {
                 currentExercisePosition++
+                exerciseList!![currentExercisePosition].setIsSelected(true)
+                exerciseAdapter!!.notifyDataSetChanged()
                 setupExerciseView()
             }
         }.start()
@@ -91,6 +93,9 @@ class ExerciseActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
 
             override fun onFinish() {
                 if (currentExercisePosition < this@ExerciseActivity.exerciseList?.size!! - 1) {
+                    exerciseList!![currentExercisePosition].setIsSelected(false)
+                    exerciseList!![currentExercisePosition].setIsCompleted(true)
+                    exerciseAdapter!!.notifyDataSetChanged()
                     setupRestView()
                 } else {
                     Toast.makeText(this@ExerciseActivity, "Congratulations!", Toast.LENGTH_SHORT)
